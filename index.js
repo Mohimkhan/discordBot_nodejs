610881bb0d42d2527483694bac75f2c74e005d11
Mohim Khan <mohimkhan652@gmail.com>
2023-12-05 21:17:34 +0600

    (feat): setup interaction for the bot

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 // MTE2MTc5MTg3NjcxMDUzNTE2OA.GdlJjy._Xwi1enkyNq3SZX5u6hbEcoyDa2tkcdc4Ga8tM
+import dotenv from 'dotenv';
+dotenv.config();
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', (message)=>{
+client.on('messageCreate', (message) => {
     if (message.author.bot) return;
     message.reply({
         content: 'Hi from bot'
     });
 })
 
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+    console.log(interaction);
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('pong!!!');
+    }
+})
 
-client.login('MTE2MTc5MTg3NjcxMDUzNTE2OA.GNite9.tCk27rTyi513N9vWlAjQ35d3FDcuFOfLdLjgAA');
+
+client.login(process.env.TOKEN);
